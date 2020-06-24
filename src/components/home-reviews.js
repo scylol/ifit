94e0d37d7d17fd0273b2ff69b5721a8534c2bf84
Donald Dobbin <djd3dobbin@gmail.com>
2020-06-24 00:24:33 -0400

    added arrows for reviews

--- src/components/home-reviews.js
+++ src/components/home-reviews.js
@@ -4,6 +4,17 @@ import HomeReviewCard from "./home-review-card";
 export default function HomeReviews() {
   return (
     <div className="home__reviews">
+      <div className="home__review__card home__review__card--partialFirst">
+        <img
+          className="home__review__card__oval home__review__card__oval--left"
+          src="images/oval.png"
+        />
+        <img
+          className="home__review__card__arrow home__review__card__arrow--left"
+          src="images/left-arrow.png"
+        />
+      </div>
+
       <HomeReviewCard
         src="/images/gear-junkie-logo.svg"
         text='"You focus on putting in the work, and the technology handles the rest."'
@@ -24,6 +35,16 @@ export default function HomeReviews() {
         text='"You focus on putting in the work, and the technology handles the rest."'
         alt="Gear Junkies"
       />
+      <div className="home__review__card home__review__card--partialLast">
+        <img
+          className="home__review__card__oval home__review__card__oval--right"
+          src="images/oval.png"
+        />
+        <img
+          className="home__review__card__arrow home__review__card__arrow--right"
+          src="images/right-arrow.png"
+        />
+      </div>
     </div>
   );
 }
